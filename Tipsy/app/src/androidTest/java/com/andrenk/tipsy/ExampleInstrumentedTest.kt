d5d98bd863ea55f8015c2a211ca3c4290c4516d7
Andreas Notokuusmo <andreasnotokusumo23@gmail.com>
2022-06-17 21:17:16 +0700

    :sparkles: Tipsy Material UI Update

--- Tipsy/app/src/androidTest/java/com/andrenk/tipsy/ExampleInstrumentedTest.kt
+++ Tipsy/app/src/androidTest/java/com/andrenk/tipsy/ExampleInstrumentedTest.kt
@@ -1,24 +1,39 @@
 package com.andrenk.tipsy
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.CoreMatchers.containsString
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
+import org.junit.Rule
+import androidx.test.espresso.matcher.ViewMatchers.withId as withId1
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class TipCalculatorTest {
+
+    //Prompt compiler to run this Activity before any testing
+    @get: Rule
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+
+    //
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.andrenk.tipsy", appContext.packageName)
+    fun calculate20PercentTip(){
+        onView(withId1(R.id.cost_of_service))
+            .perform(typeText("50"))
+            .perform(ViewActions.closeSoftKeyboard())
+
+        onView(withId1(R.id.calculate_btn))
+            .perform(click())
+
+        onView(withId1(R.id.text_result))
+            .check(matches(withText(containsString("10"))))
     }
 }
